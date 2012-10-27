7d03a920bdf25d21aca85cc4dadca2783152ee9d
Jean-Nicholas <nick.hould@gmail.com>
2012-10-26 20:14:36 -0500

    Improve test suite for static pages

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe ApplicationHelper do
+	describe "full title" do 
+
+		it "should return only base title" do
+			full_title("").should == "Ruby on Rails Tutorial Sample App"
+		end
+
+		it "should not return a base title" do
+			full_title(" ").should_not =~ /\|/
+		end
+
+		it "should include page title" do
+			full_title("foo").should =~ /foo/
+		end
+
+		it "should include base title" do
+			full_title("foo").should =~ /^Ruby on Rails Tutorial Sample App/
+		end		
+	end
+end
